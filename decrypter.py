a60fdbd4b3a83d40ad6b69d0b78cd359fa7c982d
diegosalvador <41595135+diegosalvador@users.noreply.github.com>
2023-11-30 09:39:31 -0400

    Add files via upload

--- decrypter.py
+++ decrypter.py
@@ -2,7 +2,7 @@ import os
 import pyaes
 
 ## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
+file_name = "arquivo-user.txt.ransomwaretroll"
 file = open(file_name, "rb")
 file_data = file.read()
 file.close()
@@ -16,7 +16,7 @@ decrypt_data = aes.decrypt(file_data)
 os.remove(file_name)
 
 ## criar o arquivo descriptografado
-new_file = "teste.txt"
+new_file = "arquivo_USER.txt"
 new_file = open(f'{new_file}', "wb")
 new_file.write(decrypt_data)
 new_file.close()
--- encrypter.py
+++ encrypter.py
@@ -2,7 +2,7 @@ import os
 import pyaes
 
 ## abrir o arquivo a ser criptografado
-file_name = "teste.txt"
+file_name = "arquivo-user.txt"
 file = open(file_name, "rb")
 file_data = file.read()
 file.close()
